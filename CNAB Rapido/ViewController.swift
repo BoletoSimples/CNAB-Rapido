33d49847718c1bd94238ead7d19d25d34b61a786
Rafael Lima <contato@rafael.adm.br>
2015-06-04 15:43:35 -0300

    Initial Commit

---
+++ CNAB Rapido/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CNAB Rapido
+//
+//  Created by Rafael Lima on 6/4/15.
+//  Copyright (c) 2015 br.com.boletosimples. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
