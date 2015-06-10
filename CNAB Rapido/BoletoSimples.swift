235290ceb4971c1f70452c9f018aed9313984ad0
Rafael Lima <contato@rafael.adm.br>
2015-06-10 19:52:35 -0300

    removes Alamofire and use AFHTTPRequestOperationManager

--- CNAB Rapido/BoletoSimples.swift
+++ CNAB Rapido/BoletoSimples.swift
@@ -7,73 +7,53 @@
 //
 
 import Foundation
-import Alamofire
+
+let NSURLRequestReloadIgnoringLocalCacheData = 1
 
 class BoletoSimples {
     
-    static var accessToken = ""
-    static var credential: NSURLCredential = NSURLCredential()
+    static var manager = AFHTTPRequestOperationManager()
     
     class func configure(accessToken: String!) {
-        self.accessToken = accessToken
-        self.credential = NSURLCredential(user: self.accessToken, password: "X", persistence: .ForSession)
-        Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders = [
-            "User-Agent": "CNAB Rápido (contato@boletosimples.com.br)"
-        ]
+        NSLog("BoletoSimples.configured called")
+
+        // Set credentials
+        var credential = NSURLCredential(user: accessToken, password: "X", persistence: .None)
+
+        // Configure manager
+        self.manager.credential = credential
+        self.manager.requestSerializer.setValue("CNAB Rápido (contato@boletosimples.com.br)", forHTTPHeaderField: "User-Agent")
 
     }
     
-    class func userInfo(completionHandler: (AnyObject?) -> Void) -> Void {
-        Alamofire.request(.GET, "https://sandbox.boletosimples.com.br/api/v1/userinfo")
-            .authenticate(usingCredential: self.credential)
-            .responseJSON {
-                (request, response, json, error) in
-                if(json != nil) { var json = JSON(json!); }
+    class func userInfo(completionHandler: (JSON) -> Void) -> Void {
+        self.manager.GET("https://sandbox.boletosimples.com.br/api/v1/userinfo", parameters: [],
+            success: { (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) in
+                NSLog("Userinfo success: " + operation.responseString)
+                var json = JSON(operation.responseString)
                 completionHandler(json)
-        }
+            },
+            failure: { (operation: AFHTTPRequestOperation!, error: NSError!) in
+                NSLog("Userinfo error: " + operation.responseString)
+                NSLog(operation.responseString)
+                completionHandler(nil)
+        })
+
     }
     
     class func uploadFile(fileToUpload: NSURL, completionHandler: (AnyObject?) -> Void) -> Void {
         var fileContent = String(contentsOfFile: fileToUpload.path!, encoding: NSUTF8StringEncoding, error: nil)
         let fileData = (fileContent! as NSString).dataUsingEncoding(NSUTF8StringEncoding)
-//        Alamofire.upload(.POST, "https://sandbox.boletosimples.com.br/api/v1/cnabs", file: fileToUpload)
-//            .authenticate(usingCredential: self.credential)
-//            .responseJSON {
-//                (request, response, json, error) in
-//                if(json != nil) { var json = JSON(json!); }
-//                if(response?.statusCode == 200) {
-//                    completionHandler(json)
-//                }
-//                else {
-//                    NSLog("Error uploading " + fileToUpload.path!)
-//                    NSLog(json!.description)
-//                }
-//
-//        }
-        let manager = AFHTTPRequestOperationManager()
-        manager.credential = self.credential
-        manager.requestSerializer.setValue("CNAB Rápido (contato@boletosimples.com.br)", forHTTPHeaderField: "User-Agent")
-
-//        var params = [
-//            "cnab": [
-//                "bank_billet_account_id": "1"
-//            ]
-//        ]
-        
-        manager.POST("https://sandbox.boletosimples.com.br/api/v1/cnabs", parameters: [],
+        self.manager.POST("https://sandbox.boletosimples.com.br/api/v1/cnabs", parameters: [],
             constructingBodyWithBlock: { (data: AFMultipartFormData!) in
-                println("")
-                var res: Void = data.appendPartWithFileData(fileData, name: "cnab[file]", fileName: fileToUpload.lastPathComponent!, mimeType: "text/plain")
-//                (data, name: "cnab[file]", filename: fileToUpload.lastPathComponent!, error: nil)
-                println("was file added properly to the body? \(res)")
+                data.appendPartWithFileData(fileData, name: "cnab[file]", fileName: fileToUpload.lastPathComponent!, mimeType: "text/plain")
             },
             success: { (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) in
-                println("Yes thies was a success")
-                NSLog(responseObject! as! String)
+                NSLog("Arquivo enviado com sucesso: " + fileToUpload.lastPathComponent!)
             },
             failure: { (operation: AFHTTPRequestOperation!, error: NSError!) in
+                NSLog("Erro ao enviar o arquivo: " + fileToUpload.lastPathComponent!)
                 NSLog(operation.responseString)
-                println("We got an error here.. \(error.localizedDescription)")
         })
     }
 
